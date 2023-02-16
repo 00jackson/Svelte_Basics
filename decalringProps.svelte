edb07ef260d632c3e5563bef4080f7aab94b1fae
Jackson Kujur <95626883+00jackson@users.noreply.github.com>
2023-02-16 15:40:35 +0530

    Create decalringProps.svelte

---
+++ decalringProps.svelte
@@ -0,0 +1,13 @@
+//we need to declare properties, generally shortened to 'props'. In Svelte, we do that with the export keyword. Edit the Nested.svelte component:
+
+<script>
+	export let answer;
+</script>
+//
+
+<script>
+	import Nested from './Nested.svelte';
+</script>
+
+<Nested answer={42}/>
+
